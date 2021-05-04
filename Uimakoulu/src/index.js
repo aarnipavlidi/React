eb707a1fe1271b283550059753fa1a6926a98b85
aarnipavlidi <55801258+aarnipavlidi@users.noreply.github.com>
2021-05-04 19:32:35 +0300

    uimakoulu_ver_3
    Changing navbar color and adding bottom-border-color. For now using !important method, to override bootstrap navbar color.  Didn't get it worked without it!

--- Uimakoulu/src/index.js
+++ Uimakoulu/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react' // Sovellus ottaa "react" nimisen kirjaston käyttöönsä.
 import ReactDOM from 'react-dom' // Sovellus ottaa "react-dom" nimisen kirjaston käyttöönsä.
 import App from './App' // Sovellus ottaa "App.js" tiedoston sisällön käyttöönsä.
+import 'bootstrap/dist/css/bootstrap.css'
 import './index.css' // Sovellus ottaa "index.css" (tyylitiedosto) tiedoston sisällön käyttöönsä.
 
 ReactDOM.render(<App />, document.getElementById('root'))
