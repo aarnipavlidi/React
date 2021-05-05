ff0aca6380781dd6c9717151a80b0234ed2aed34
aarnipavlidi <55801258+aarnipavlidi@users.noreply.github.com>
2021-05-05 16:57:23 +0300

    uimakoulu_ver_6
    Added Google maps (embedded version for now) to the website, it's responsive, but doesn't look good/clean on desktop view. On mobile it looks good, tried to fix/find solution, but wasn't able to. Will fix it later!

--- Uimakoulu/src/App.js
+++ Uimakoulu/src/App.js
@@ -34,6 +34,17 @@ const App = () => { // Sovellus alkaa tästä...
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dignissim suspendisse in est ante in nibh. Lobortis feugiat vivamus at augue eget arcu dictum. Aliquam ut porttitor leo a diam sollicitudin tempor id. Non curabitur gravida arcu ac tortor dignissim convallis aenean et. Massa tincidunt nunc pulvinar sapien et. Ac turpis egestas integer eget aliquet. Nec ultrices dui sapien eget mi proin sed libero. Rutrum quisque non tellus orci ac auctor augue mauris. Commodo nulla facilisi nullam vehicula ipsum. Congue quisque egestas diam in arcu. Et malesuada fames ac turpis. Id cursus metus aliquam eleifend mi in. Non blandit massa enim nec dui nunc mattis enim ut. Ultrices in iaculis nunc sed augue lacus viverra vitae congue. Arcu ac tortor dignissim convallis aenean et tortor at. Nunc non blandit massa enim nec dui nunc. Eget mi proin sed libero enim. Morbi tincidunt augue interdum velit euismod.</p>
         </div>
 
+        <div className="embed-responsive embed-responsive-21by9 justify-content-center">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1685.5863428945438!2d25.495589216336505!3d65.00924905030993!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4681cd53839d48e5%3A0x271114ad31124a78!2sOulun%20Uimahalli!5e0!3m2!1sen!2sfi!4v1620219229195!5m2!1sen!2sfi"
+            width="400"
+            height="300"
+            style={{ border: 0 }}
+            className="embed-responsive-item"
+            allowfullscreen=""
+            loading="lazy"
+          />
+        </div>
+
       </div>
 
         <footer className="container-fluid custom-footer">
